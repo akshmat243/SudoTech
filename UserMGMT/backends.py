8564a77597c71e3f4bef98c87b3480e11ba69574
Aakash kumawat <akshmat243@gmail.com>
2025-05-09 18:53:36 +0530

    admin approval

--- UserMGMT/backends.py
+++ UserMGMT/backends.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from .models import User
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
